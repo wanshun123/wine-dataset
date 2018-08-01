fb6d1b9a4f52c0737ef50b9f3bc509a5677f811d
wanshun123 <citexs@gmail.com>
2018-08-01 16:23:42 +0800

    Update wine.r

--- wine.r
+++ wine.r
@@ -11,18 +11,21 @@ ggscatter(df, x = "quality", y = "alcohol",
           cor.coef = TRUE, cor.method = "pearson")
 # count of each rating
 ggplot(df, aes(x = quality)) + geom_bar()
-# turn quality into a factor?
-df$quality <- as.factor(df$quality)
-# test and train
+# index of rows
 index <- sample(1:nrow(df),size = 0.8*nrow(df))
-train <- df[index,]
-test <- df[-index,]
+#train <- df[index,]
+#test <- df[-index,]
 
 ### random forest
 
 library(randomForest)
-rf_model <- randomForest(quality ~., data = train)
-rf_prediction <- predict(rf_model, test)
+# for a random forest, quality has to be a factor
+df_rf <- df
+df_rf$quality <- as.factor(df_rf$quality)
+train_rf <- df_rf[index,]
+test_rf <- df_rf[-index,]
+rf_model <- randomForest(quality ~., data = train_rf)
+rf_prediction <- predict(rf_model, test_rf)
 sqrt(mean((rf_prediction-test$quality)^2)) # pointless if quality is a factor
 # get the caret and e1071 package to make a confusion matrix
 library(caret)
@@ -31,33 +34,51 @@ rf_matrix <- confusionMatrix(rf_prediction, test$quality)
 
 ### linear model
 
-# linear model doesn't work on factors - so make "good" and "bad" wine column (will be factor even though it's logical 1 or 0)
-df2 <- read.csv("winequality-red.csv")
-df2$quality <- ifelse(df2$quality>5,1,0) # df2$quality has to be integer - won't work if already converted to factor
-df2$quality <- as.factor(df2$quality)
-train2 <- df2[index,]
-test2 <- df2[-index,]
-lm_model <- glm(quality ~., data = train2, family = "binomial")
-lm_prediction <- predict(lm_model, test2, type = "response")
+# linear model doesn't work on factors or integers - so make "good" and "bad" wine column (will be factor even though it's logical 1 or 0)
+df_lm <- df
+df_lm$quality <- ifelse(df_lm$quality>5,1,0) # df_lm$quality has to be integer - won't work if already converted to factor
+df_lm$quality <- as.factor(df_lm$quality)
+train_lm <- df_lm[index,]
+test_lm <- df_lm[-index,]
+lm_model <- glm(quality ~., data = train_lm, family = "binomial")
+lm_prediction <- predict(lm_model, test_lm, type = "response")
 lm_prediction <- round(lm_prediction, digits = 0)
 #the below or round(), either way
 #lm_prediction <- ifelse(lm_prediction>0.5,1,0)
 lm_prediction <- as.factor(lm_prediction)
-lm_matrix <- confusionMatrix(lm_prediction, test2$quality)
-# random forest on binomial "good" or "bad" df2 - same thing
-rf2_model <- randomForest(quality ~., data = train2)
-rf2_prediction <- predict(rf2_model, test2)
-rf2_matrix <- confusionMatrix(rf2_prediction, test2$quality)
+lm_matrix <- confusionMatrix(lm_prediction, test_lm$quality)
+
+# random forest on binomial "good" or "bad" df_lm - same thing
+rf2_model <- randomForest(quality ~., data = train_lm)
+rf2_prediction <- predict(rf2_model, test_lm)
+rf2_matrix <- confusionMatrix(rf2_prediction, test_lm$quality)
 
 ### stepwise regression
-# df as loaded straight from cv, quality not changed to factors
+# df as loaded straight from cv, quality not changed to factors (?)
 
-df <- read.csv("winequality-red.csv")
+df_sw <- df
+df_sw$quality <- as.factor(df_sw$quality)
 
-null_model <- glm(quality ~ 1, data = train)
-full_model <- glm(quality ~ ., data = train)
+null_model <- glm(quality ~ 1, data = df_sw)
+full_model <- glm(quality ~ ., data = df_sw)
 step_model <- step(null_model, scope = list(lower = null_model, upper = full_model), direction = "forward")
-stepwise_purchase_prediction <- predict(step_model)
+stepwise_purchase_prediction <- predict(step_model, type = "response")
 stepwise_purchase_prediction <- round(stepwise_purchase_prediction, digits = 0)
 stepwise_purchase_prediction <- as.factor(stepwise_purchase_prediction)
-sw_matrix <- confusionMatrix(stepwise_purchase_prediction, train$quality)
+sw_matrix <- confusionMatrix(stepwise_purchase_prediction, df_sw$quality)
+
+#library(pROC)
+#stepwise_purchase_prediction <- predict(step_model, type = "response")
+#ROC <- multiclass.roc(df_sw$quality, stepwise_purchase_prediction)
+
+### stepwise regression to determine good or bad
+
+df2_sw <- df
+df2_sw$quality <- ifelse(df_sw$quality>5,1,0)
+
+null_model <- glm(quality ~ 1, data = df2_sw)
+full_model <- glm(quality ~ ., data = df2_sw)
+step_model <- step(null_model, scope = list(lower = null_model, upper = full_model), direction = "forward")
+stepwise_purchase_prediction <- predict(step_model, type = "response")
+ROC <- roc(df2_sw$quality, stepwise_purchase_prediction)
+auc(ROC)
